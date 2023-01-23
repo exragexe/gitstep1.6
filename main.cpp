ed4771716436e554c14dc55de63eb4dc27918d42
yaros <yaroslavdisiak@gmail.com>
2023-01-23 18:47:24 +0200

    ex4donepw

--- main.cpp
+++ main.cpp
@@ -1,1 +1,24 @@
 
+
+#include <iostream>
+
+using namespace std;
+void Exercise() {
+    const int size = 6;
+    int arr[size];
+    int sumaa=0;
+    for (size_t i = 0; i < size; i++)
+    {
+        cout << "Enter number for masive(6 num): ";
+        cin >> arr[i];
+    }
+    for (int i = size-1; i >=0; i--) {
+        cout << arr[i]<<" ";
+
+    }
+
+}
+int main() {
+    Exercise();
+    return 0;
+}
