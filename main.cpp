6235ab68e0d8875ea2a944fccf07f58e04a00dff
yaros <yaroslavdisiak@gmail.com>
2023-01-23 21:27:42 +0200

    ex3donehw

--- main.cpp
+++ main.cpp
@@ -1,1 +1,39 @@
 
+
+#include <iostream>
+#include <time.h>
+#include <Windows.h>
+
+using namespace std;
+void Exercise() {
+    srand(time(NULL));
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    const int size = 6;
+    int arr[size];
+    int min, max;
+    min = arr[0];
+
+    for (size_t i = 1; i < size; i++)
+    {
+        arr[i]=rand()%100+1;
+        cout <<arr[i]<<" ";
+
+        if(i == 0)
+        {
+            min = max = arr[i];
+        }
+        if(arr[i]>max){
+            max = arr[i];
+        }
+        if(arr[i]<min){
+            min = arr[i];
+        }
+    }
+    cout <<endl;
+    cout <<"Min: "<< min<<" Max: "<< max;
+
+}
+int main() {
+    Exercise();
+    return 0;
+}
