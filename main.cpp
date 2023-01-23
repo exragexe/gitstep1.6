ad40995f3125b418f5e51daf1ea53e311667a078
yaros <yaroslavdisiak@gmail.com>
2023-01-23 18:52:29 +0200

    ex5donepw

--- main.cpp
+++ main.cpp
@@ -1,1 +1,21 @@
 
+
+#include <iostream>
+
+using namespace std;
+void Exercise() {
+    const int size = 6;
+    int arr[size];
+    double sumaa=0;
+    for (size_t i = 1; i < size; i++)
+    {
+        cout << "Enter side of pentagon: ";
+        cin >> arr[i];
+        sumaa += arr[i];
+    }
+    cout << "Perimetr: " << sumaa;
+}
+int main() {
+    Exercise();
+    return 0;
+}
