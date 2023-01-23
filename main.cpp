c1b719a5d9e87c625053ca8cfaefec9cd099bcb7
yaros <yaroslavdisiak@gmail.com>
2023-01-23 18:22:01 +0200

    ex3donepw

--- main.cpp
+++ main.cpp
@@ -1,1 +1,21 @@
 
+
+#include <iostream>
+
+using namespace std;
+void Exercise() {
+    const int size = 6;
+    int arr[size];
+    double sumaa=0;
+    for (size_t i = 1; i < size; i++)
+    {
+        cout << "Enter money for month: ";
+        cin >> arr[i];
+        sumaa += arr[i];
+    }
+    cout << "Suma money for 6 month: " << sumaa;
+}
+int main() {
+    Exercise();
+    return 0;
+}
